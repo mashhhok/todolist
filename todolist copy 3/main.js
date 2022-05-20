6ef7f79e9bb21f8204414733945fce5fcb189515
Mariia <71460862+mashhhok@users.noreply.github.com>
2022-05-20 13:40:20 +0200

    Re-made functions to the global scope

--- todolist copy 3/main.js
+++ todolist copy 3/main.js
@@ -2,102 +2,129 @@
 
 const addTaskButton = document.getElementById('addTask');
 const lineTaskInput = document.getElementById('taskInput');
-const toDoList = document.getElementById('tasklist');
+const toDoList = document.getElementById('task-list');
 const clearing = document.getElementById('clearAll');
 const checkBoxCover = document.getElementById('clearAll');
 
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
-!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
 function Task(description) {
-	this.description = description;
-	this.completed = false
+    this.description = description;
+    this.completed = false
 }
 
 let toDoElem = [];
 
+
 const updateHTML = () => {
-		toDoList.innerHTML = "";
-		if(tasks.length > 0){
-			tasks.forEach((item, index) => {
-				let taskLine = document.createElement('li');
-	taskLine.classList.add("task-item");
+    toDoList.innerHTML = "";
+    if (tasks.length > 0) {
+        tasks.forEach((item, index) => {
+            let taskLine = completedItem(item);
+			
+			let deleteButton = createButton(index, taskLine);
 
-	let deleteButton = document.createElement('button');
-	deleteButton.classList.add("delete");
-	deleteButton.innerHTML = '<input type="image" class="trash" src="pics/trash.svg" name="delete" alt="delete">';
-	deleteButton.onclick = function() {deleteTask(index)};
+            let taskLabel = document.createElement('label');
+            taskLabel.classList.add("tasks");
 
-	let taskLabel = document.createElement('label');
-	taskLabel.classList.add("tasks");
+            let checkBoxComplete = tickCheckbox(item, index);
 
-	let checkBoxComplete = document.createElement('input');
-	checkBoxComplete.classList.add("complete");
-	checkBoxComplete.type = 'checkbox';
-	checkBoxComplete.onclick = function() {completeTask(index)};
+            let taskName = createTask(item);
 
-	let taskName = document.createElement('span');
-	taskName.classList.add("task-name");
-	taskName.innerHTML = item.description;
+            let checkMark = document.createElement('span');
+            checkMark.classList.add("checkmark");
 
-	let checkMark = document.createElement('span');
-	checkMark.classList.add("checkmark");
+            taskLabel.appendChild(checkBoxComplete);
+            taskLabel.appendChild(taskName);
+            taskLabel.appendChild(checkMark);
 
-	taskLabel.appendChild(checkBoxComplete);
-	taskLabel.appendChild(taskName);
-	taskLabel.appendChild(checkMark);
+			taskLine.appendChild(deleteButton);
+            taskLine.appendChild(taskLabel);
 
-	taskLine.appendChild(deleteButton);
-	taskLine.appendChild(taskLabel);
+            toDoList.appendChild(taskLine);
 
-	toDoList.appendChild(taskLine);
-	
-			})
-			toDoElem = document.querySelectorAll('.task-item'); 
-		}
-}
+        })
+        toDoElem = document.querySelectorAll('.task-item');
+    };
+};
+
+
 updateHTML();
-	const updateLocal = () => {
-	localStorage.setItem('tasks', JSON.stringify(tasks));
+const updateLocal = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 const completeTask = index => {
-	tasks[index].completed = !tasks[index].completed; 
-	if(tasks[index].completed) {
-		toDoElem[index].classList.add('done'); 
-	}
-	else {
-		toDoElem[index].classList.remove('done');
-	}
-	updateLocal();
-	updateHTML();
+    tasks[index].completed = !tasks[index].completed;
+    updateLocal();
+    updateHTML();
 }
 
 addTaskButton.addEventListener('click', () => {
-	tasks.push(new Task(lineTaskInput.value));
-	updateLocal();
-	updateHTML();
-	lineTaskInput.value = '';
+    tasks.push(new Task(lineTaskInput.value));
+    updateLocal();
+    updateHTML();
+    lineTaskInput.value = '';
 });
 
 const deleteTask = index => {
-	tasks.splice(index, 1);
-	updateLocal();
-	updateHTML();
+    tasks.splice(index, 1);
+    updateLocal();
+    updateHTML();
 }
 
 clearing.addEventListener("click", () => {
-	tasks = [];
-	updateLocal();
-	updateHTML();
+    tasks = [];
+    updateLocal();
+    updateHTML();
 });
 
-document.querySelector('#taskInput').addEventListener('keypress', function (e) {
+document.querySelector('#taskInput').addEventListener('keypress', function(e) {
     if (e.key === 'Enter') {
-      tasks.push(new Task(lineTaskInput.value));
-	updateLocal();
-	updateHTML();
-	lineTaskInput.value = '';
+        tasks.push(new Task(lineTaskInput.value));
+        updateLocal();
+        updateHTML();
+        lineTaskInput.value = '';
     }
 });
+
+function completedItem(item) {
+    let taskLine = document.createElement('li');
+    taskLine.classList.add("task-item");
+    if (item.completed) {
+        taskLine.classList.add("done");
+    } else {
+        taskLine.classList.remove("done");
+    };
+    return taskLine;
+}
+
+function createTask(item) {
+    let taskName = document.createElement('span');
+    taskName.classList.add("task-name");
+    taskName.innerHTML = item.description;
+    return taskName;
+}
+
+function tickCheckbox(item, index) {
+    let checkBoxComplete = document.createElement('input');
+    checkBoxComplete.classList.add("complete");
+    checkBoxComplete.type = 'checkbox';
+    checkBoxComplete.checked = item.completed;
+    checkBoxComplete.addEventListener('click', function () {
+        completeTask(index);
+    });
+    return checkBoxComplete;
+}
+
+function createButton(index, taskLine) {
+    let deleteButton = document.createElement('button');
+    deleteButton.classList.add("delete");
+    deleteButton.innerHTML = '<img class="trash" src="pics/trash.svg" alt="delete">';
+    deleteButton.addEventListener('click', function () {
+        deleteTask(index);
+    });
+    taskLine.appendChild(deleteButton);
+    return deleteButton;
+}
