4262c28e9202ad77ac9b19bfb984718211290d9e
Mariia <71460862+mashhhok@users.noreply.github.com>
2022-05-30 16:37:15 +0200

    Re-made naming and functions

--- mochatest/test/testApp.js
+++
@@ -1,8 +0,0 @@
-// const assert = require('chai').assert
-// const app = require('../../todolist copy 3/main')
-
-// describe('Main', function(){
-//     it('app should update list', function()){
-//         assert.equal(main(updateList));
-//     }
-// })
